25824bf1f358e7f287234ba729db8e5ead402413
Sujan <sujanramesh91766@gmail.com>
2024-06-06 11:47:49 +0530

    Day-4 (Learning Java Basic Keywords)

---
+++ Fundamentals/src/in/basics/FirstProgram.java
@@ -0,0 +1,9 @@
+package in.basics;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		//Static is always related to class and can be accessed only using the class name and object name.
+		System.out.print("First Program");
+	}
+}
